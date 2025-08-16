c1b6da831d1dd601662a3559a268b6920b5f7f00
Soham Katore <rupali.nkatore9@gmail.com>
2025-08-16 14:53:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// Bootstrap Carousel auto slide
+// Auto slide every 3 seconds
 const carousel = document.querySelector('#shopCarousel');
-if(carousel){
-  const bsCarousel = new bootstrap.Carousel(carousel,{
-    interval:4000,
-    ride:'carousel'
+if (carousel) {
+  const bsCarousel = new bootstrap.Carousel(carousel, {
+    interval: 3000,
+    ride: 'carousel'
   });
 }
-
-// Optional: Add scroll animations for elements
-window.addEventListener('scroll',()=>{
-  const elements = document.querySelectorAll('[data-aos]');
-  elements.forEach(el=>{
-    const rect = el.getBoundingClientRect();
-    if(rect.top < window.innerHeight - 100){
-      el.classList.add('aos-animate');
-    }
-  });
-});
