eb54959155cb56f145ab782c42bfb3d55ae869e1
Soham Katore <rupali.nkatore9@gmail.com>
2025-08-16 13:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// You can add interactivity here
-// Example: Alert on page load
-window.onload = function() {
-  console.log("Welcome to My Shop Website!");
-};
+// Simple welcome message in console
+console.log("Welcome to My Xerox & Printing Shop Website!");
+
+// Smooth scroll effect for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
